8f8dccd293f2486a8a5b4e05c7199bf699379b75
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-08-09 11:07:08 -0500

    OSPB-20: Implement database schema and migrations

--- app/db/models.py
+++ app/db/models.py
@@ -141,7 +141,7 @@ class Comment(Base):
     __tablename__ = 'comments'
 
     id = Column(Integer, primary_key=True, index=True)
-    content = Column(Text, nullable=False)
+    text = Column(Text, nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     claim_id = Column(Integer, ForeignKey('claims.id'))
     verification_id = Column(Integer, ForeignKey('verifications.id'))
